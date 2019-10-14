904a2e5229596690481ba4cb15f13c89a5d2d3b6
Joe Foster <nfcnorthafterhours@gmail.com>
2019-10-14 06:46:28 -0500

    initial commit

---
+++ XEx04Quotation/Confirm.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Confirm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            if (Session["salesPrice"] == null)
+            {
+                lblDiscountAmount.Text = null;
+                lblSalesPrice.Text = null;
+                lblTotalPrice.Text = null;
+            }
+            else
+            {
+                String discountAmount = (String)Session["discountAmount"];
+                String salesPrice = (String)Session["salesPrice"];
+                String totalPrice = (String)Session["totalPrice"];
+
+
+                lblDiscountAmount.Text = discountAmount;
+                lblSalesPrice.Text = salesPrice;
+                lblTotalPrice.Text = totalPrice;
+
+            }
+
+           
+
+        }
+
+        protected void btnReturn_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text == "" || txtName.Text == "")
+            Response.Redirect("Default.aspx");
+        }
+
+        protected void btnQuotation_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                String email = txtEmail.Text;
+                String name = txtName.Text;
+
+                lblMessage.Text = "Quotation sent to " + name + " at " + email + ".";
+                Session.Remove("txtEmail");
+                Session.Remove(name);
+
+            }
+            
+
+        }
+    }
+}
---
+++ XEx04Quotation/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            String discountAmount = (String)Session["discountAmount"];
+            String salesPrice = (String)Session["salesPrice"];
+            String totalPrice = (String)Session["totalPrice"];
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                decimal salesPrice = Convert.ToDecimal(txtSalesPrice.Text);
+                decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text) / 100;
+
+                decimal discountAmount = salesPrice * discountPercent;
+                decimal totalPrice = salesPrice - discountAmount;
+
+                lblDiscountAmount.Text = discountAmount.ToString("c");
+                lblTotalPrice.Text = totalPrice.ToString("c");
+
+                Session.Add("discountAmount", discountAmount.ToString("c"));
+                Session.Add("salesPrice", salesPrice.ToString("c"));
+                Session.Add("totalPrice", totalPrice.ToString("c"));
+            }   
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            if (Session["salesPrice"] == null)
+            {
+                lblMessage.Text = "Click the Calculate button before you confirm.";
+            }
+            else
+            {
+                Response.Redirect("Confirm.aspx");
+            }
+           
+        }
+    }
+}
